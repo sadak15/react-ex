d6777c3e3b31fe35f385d842e382d94e54311806
sadak15 <sadakc15@gmail.com>
2026-06-01 20:07:44 +0300

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+import Usercard from './Usercard'
+
+function App() {
+  return(
+    <Usercard/>
+  )
+}
+
+export default App
---
+++ src/Usercard.jsx
@@ -0,0 +1,11 @@
+
+
+const UserCard = () => {
+    return (
+    <div>
+        <h2>User Name: Ahmed Ali</h2>
+        <p>Email: sadakc15@gmail.com</p>
+    </div>
+)
+};
+ export default UserCard
