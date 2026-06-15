eeef0b2c2599a3d795825c9049c135af9832ae02
sadak15 <sadakc15@gmail.com>
2026-06-15 21:11:35 +0300

    ex3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import { useState } from 'react'
 
-import Blog from './Blog'
+// import Blog from './Blog'
+
+import UserCard from './Usercard'
 
 function App() {
   return(
-     <Blog/>
+    <>
+
+    <UserCard 
+    Name={"Ahmed"}
+    Email={"sadakc15@gmail.com"}
+    />
+    <UserCard 
+    Name={"Ali"}
+    Email={"sadakc15@gmail.com"}
+    />
+    <UserCard 
+    Name={"Ahmed"}
+    Email={"ahmed@gmail.com"}
+    />
+    <UserCard 
+    Name={"Maryan"}
+    Email={"maryan@gmail.com"}
+    />
+
+    </>
   )
 }
 
--- src/Usercard.jsx
+++ src/Usercard.jsx
@@ -1,10 +1,24 @@
 
 
-const UserCard = () => {
+// const UserCard = () => {
+//     return (
+//     <div>
+//         <h2>User Name: Ahmed Ali</h2>
+//         <p>Email: sadakc15@gmail.com</p>
+//     </div>
+// )
+// };
+//  export default UserCard
+
+
+
+
+
+const UserCard = ({Name, Email}) => {
     return (
     <div>
-        <h2>User Name: Ahmed Ali</h2>
-        <p>Email: sadakc15@gmail.com</p>
+        <h2>User Name: {Name}</h2>
+        <p>Email: {Email}</p>
     </div>
 )
 };
