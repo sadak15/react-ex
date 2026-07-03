47e95f5222048f7bc82ac269a7a7de67d9d13aa0
sadak15 <sadakc15@gmail.com>
2026-07-03 22:56:08 +0300

    Exercise #6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 
-import ShoppingCart from './ShoppingCart'
+import  {useEffect, useState} from 'react'
 
 
 function App() {
- 
-  return(
-    <>
-    <ShoppingCart />
-    </>
-  )
+
+   const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('Hello');
+
+  useEffect(() => {
+    if (!name) {
+      document.title = 'Welcome!';
+    } else {
+      document.title = `${greeting}, ${name}`;
+    }
+  }, [name, greeting]);
+
+   return (
+    <div>
+      <h2>Enter Your Name:</h2>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <h2>Choose a Greeting:</h2>
+      <input
+        type="text"
+        value={greeting}
+        onChange={(e) => setGreeting(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App
