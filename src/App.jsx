7d7725c21ffeb06e37afd7ebeb1d36d4c37165aa
sadak15 <sadakc15@gmail.com>
2026-07-07 19:34:27 +0300

    exercise 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-
-import  {useEffect, useState} from 'react'
-
-import  MouseTracker from './MouseTracker'
-
+import Countdown from "./Countdown"
 
 function App() {
 
-   const [name, setName] = useState('');
-  const [greeting, setGreeting] = useState('Hello');
-
-  useEffect(() => {
-    if (!name) {
-      document.title = 'Welcome!';
-    } else {
-      document.title = `${greeting}, ${name}`;
-    }
-  }, [name, greeting]);
-
    return (
-    <div>
-      <h2>Enter Your Name:</h2>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <h2>Choose a Greeting:</h2>
-      <input
-        type="text"
-        value={greeting}
-        onChange={(e) => setGreeting(e.target.value)}
-       
-      />
-       <MouseTracker/>
-    </div>
-  );
+    <>
+    <Countdown/>
+    </>
+   )
 }
 
 export default App
